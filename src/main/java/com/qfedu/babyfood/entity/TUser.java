f1d870a18edfefe6f3f8c62d9ed24e232a470abd
dong <2337441196@qq.com>
2019-06-09 14:39:12 +0800

    update

--- src/main/java/com/qfedu/babyfood/entity/TUser.java
+++ src/main/java/com/qfedu/babyfood/entity/TUser.java
@@ -1,6 +1,7 @@
 package com.qfedu.babyfood.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.util.Date;
@@ -30,6 +31,7 @@ public class TUser extends Model<TUser> implements Serializable{
     /**
      * 用户名
      */
+    @TableField(value = "userName")
     private String userName;
     /**
      * 用户密码
@@ -38,6 +40,7 @@ public class TUser extends Model<TUser> implements Serializable{
     /**
      * 真实姓名
      */
+    @TableField(value = "realName")
     private String realName;
     /**
      * 地址
@@ -58,14 +61,17 @@ public class TUser extends Model<TUser> implements Serializable{
     /**
      * 宝宝id
      */
+    @TableField(value = "babyId")
     private Integer babyId;
     /**
      * 城市id
      */
+    @TableField(value = "cityId")
     private Integer cityId;
     /**
      * 积分id
      */
+    @TableField(value = "integralId")
     private Integer integralId;
     /**
      * 角色
@@ -78,6 +84,7 @@ public class TUser extends Model<TUser> implements Serializable{
     /**
      * 点赞判断
      */
+    @TableField(value = "flagTime")
     private Date flagTime;
 
 
